c7a97a5623c5e3f73e962dc51e9b6e8ba7d49005
Stacey <79818270+louyecats@users.noreply.github.com>
2023-06-30 16:57:53 -0400

    initialized new repo

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import llama from './llama.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={llama} className="App-logo" alt="logo" />
+        <h1>Hola senorita!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
