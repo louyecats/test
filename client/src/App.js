739fbcf1c786616d1cbc53d92fc3f03268f9ba50
Theresa Miles <ThereIsASmile@gmail.com>
2023-07-06 18:04:12 -0400

    Que pasa? with a fade in toggle switch.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import llama from './llama.webp';
+import React, { useState } from 'react';
 import './App.css';
 
+
+// ----- Images! ----  
+import llama from './llama.webp';
+import blue_llama from './assets/blue_llama.png';
+
+
 function App() {
+  const [isFadedIn, setIsFadedIn] = useState(false);
+
+  const handleFadeToggle = () => {
+    setIsFadedIn(!isFadedIn);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={llama} className="App-logo" alt="logo" />
         <h1>Hola senorita!</h1>
-      </header>
+        <button className="fade-button" onClick={handleFadeToggle}>Que pasa?</button>
+        <img src={blue_llama} className={`Blue_llama_css ${isFadedIn ? 'fade-in' : 'fade-out'}`} alt="Blue llama" />
+        </header>
     </div>
   );
 }
